d648444756b425fc6387aa0421db0226e023c644
PsyloXVV <andrei.milivojevic@gmail.com>
2021-04-24 14:27:07 +1200

    Added colour changing button with the javascript file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Sup World")
+//function myFunction(){
+//    console.log("Hello World")
+//}
+//myFunction()
+window.onload = () => {
+    const btn = document.getElementById("btn")
+    console.log(btn)
+    btn.innerHTML = "World"
+    btn.style = "color: white"
+    let blue= true
+    btn.onclick = () => {
+        if (blue == true){
+            console.log("colour will now be green")
+            btn.style = "background-color : green; color: white"
+            blue= false
+        }
+        else{
+            console.log("colour will now be blue")
+            btn.style = "background-color : blue; color: white"
+            blue = true
+        }
+
+    }
+    
+    
+    
+}
